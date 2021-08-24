4db109f60677794842872162160d2653d2b76bdd
AnAppleCore <994164148@qq.com>
2021-08-24 09:15:11 +0000

    add gitignore

--- model_train.sh
+++ model_train.sh
@@ -1,1 +1,3 @@
-python train.py train --in_path /root/data/barrycao/data/imagenet -o results/squeezenet1_1/ --model_arch squeezenet1_1 --ngpus 4 --workers 4 --epochs 20 --restore_epoch 2 --restore_path results/squeezenet1_1/
+#python train.py train --in_path /root/data/barrycao/data/imagenet -o results/squeezenet1_1/ --model_arch squeezenet1_1 --ngpus 4 --workers 4 --epochs 20 --restore_epoch 2 --restore_path results/squeezenet1_1/
+
+python train.py train --in_path /root/data/barrycao/data/imagenet -o results/vgg19_bn/ --model_arch vgg19_bn --ngpus 4 --workers 4 --epochs 20 --batch_size 384 --restore_epoch 9 --restore_path results/vgg19_bn/
